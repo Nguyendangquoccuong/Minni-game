7eece4d20a616568ef064e6c7ed0787267a9daf0
Nguyendangquoccuong <nguyendangquoccuong868@gmail.com>
2026-06-29 12:36:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+let number = Math.floor(Math.random() * 10) + 1;
+
 function startGame() {
-  document.getElementById("text").innerHTML =
-    "🎉 Chúc bạn chơi game vui vẻ!";
+    let guess = prompt("🎮 Đoán số từ 1 đến 10:");
+
+    if (guess == null) return;
+
+    if (parseInt(guess) === number) {
+        document.getElementById("text").innerHTML =
+        "🎉 Chính xác! Bạn thắng!";
+        number = Math.floor(Math.random() * 10) + 1;
+    } else {
+        document.getElementById("text").innerHTML =
+        "❌ Sai rồi! Số đúng là " + number + ". Nhấn nút để chơi lại.";
+        number = Math.floor(Math.random() * 10) + 1;
+    }
 }
